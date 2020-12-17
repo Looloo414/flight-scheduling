c33ca55fa44cc62f78930f7f01e7faedb759e68f
Looloo414 <alexafaulkison@gmail.com>
2020-12-16 23:03:21 -0700

    Troubleshooting Rendering Views for Schedules

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,9 @@ const User = require('../models/user');
 
 module.exports = {
     index,
-    showProfile
+    showProfile,
+    show,
+    update
 
 }
 function index(req, res) {
@@ -16,6 +18,28 @@ function showProfile(req, res) {
     User.findById(req.user._id)
     res.render("users/profile", { title: "Profile Page", user })
 }
+
+function update(req, res) {
+    User.findByIdAndUpdate(req.user._id, req.body, {new: true})
+    .then(() => {
+      res.redirect("/users/profile")
+    })
+  }
+  
+  function show(req, res) {
+    User.findById(req.params.id)
+    .then((userInfo) => {
+      Schedule.find({ added: userInfo._id })
+      .then((schedules) => {
+        res.render("users/show", {
+          title: "User Details",
+          userInfo,
+          user: req.user,
+          schedules
+        })
+      })
+    })
+  }
 
 
 
